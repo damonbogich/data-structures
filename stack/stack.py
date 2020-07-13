a774955cc13d7bbed8ac6a4b3bb8874fe57df6dc
damonbogich <damonbogich@gmail.com>
2020-07-13 18:04:47 -0400

    stack working with linked list implementation now

--- stack/stack.py
+++ stack/stack.py
@@ -11,36 +11,161 @@ return elements in Last In First Out order.
    implementing a Stack?
 """
 
-#Python list implementation:
-class Stack:
-    def __init__(self):
-        self.size = 0
-        self.storage = []
-
-    def __len__(self):
-        return len(self.storage)
-
-    def push(self, value):
-        #Add to the top of the stack
-        self.storage.insert(0, value)
-
-    def pop(self):
-        #removes from the top of the stack
-        if len(self.storage) == 0:
-            return None
-        else:
-            return self.storage.pop(0)
-
+##Python list implementation:
 # class Stack:
 #     def __init__(self):
 #         self.size = 0
-#         # self.storage = ?
+#         self.storage = []
 
 #     def __len__(self):
-#         pass
+#         return len(self.storage)
 
 #     def push(self, value):
-#         pass
+#         #Add to the top of the stack
+#         self.storage.insert(0, value)
 
 #     def pop(self):
-#         pass
+#         #removes from the top of the stack
+#         if len(self.storage) == 0:
+#             return None
+#         else:
+#             return self.storage.pop(0)
+
+
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_to_tail(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.set_next(new_node)
+            self.tail = new_node
+
+    def remove_head(self):
+        # return None if there is no head (i.e. the list is empty)
+        if not self.head:
+            return None
+        # if head has no next, then we have a single element in our list
+        if not self.head.get_next():
+            # get a reference to the head
+            head = self.head
+            # delete the list's head reference
+            self.head = None
+            # also make sure the tail reference doesn't refer to anything
+            self.tail = None
+            # return the value
+            return head.get_value()
+        # otherwise we have more than one element in our list
+        value = self.head.get_value()
+        # set the head reference to the current head's next node in the list
+        self.head = self.head.get_next()
+        return value
+
+    def remove_tail(self):
+        if not self.head:
+            return None
+        
+        if self.head is self.tail:
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return value
+        
+        current = self.head
+
+        while current.get_next() is not self.tail:
+            current = current.get_next()
+
+        value = self.tail.get_value()
+        self.tail = current
+        self.tail.set_next(None)
+        return value
+
+    def contains(self, value):
+        if not self.head:
+            return False
+
+        # Recursive solution
+        # def search(node):
+        #   if node.get_value() == value:
+        #     return True
+        #   if not node.get_next():
+        #     return False
+        #   return search(node.get_next())
+        # return search(self.head)
+    
+        # get a reference to the node we're currently at; update this as we traverse the list
+        current = self.head
+        # check to see if we're at a valid node 
+        while current:
+            # return True if the current value we're looking at matches our target value
+            if current.get_value() == value:
+                return True
+            # update our current node to the current node's next node
+            current = current.get_next()
+        # if we've gotten here, then the target node isn't in our list
+        return False
+        
+    def get_max(self):
+        if not self.head:
+            return None
+        # reference to the largest value we've seen so far
+        max_value = self.head.get_value()
+        # reference to our current node as we traverse the list
+        current = self.head.get_next()
+        # check to see if we're still at a valid list node
+        while current:
+            # check to see if the current value is greater than the max_value
+            if current.get_value() > max_value:
+                # if so, update our max_value variable
+                max_value = current.get_value()
+            # update the current node to the next node in the list
+            current = current.get_next()
+        return max_value
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        if self.storage.head is None:
+            return 0
+        elif self.storage.head.next_node is None:
+            return 1
+        else:
+            length = 1
+            current_node = self.storage.head
+            while current_node.next_node is not None:
+                length += 1
+                current_node = current_node.next_node
+            return length
+
+    def push(self, value):
+        return self.storage.add_to_tail(value)
+
+    def pop(self):
+        return self.storage.remove_tail()
