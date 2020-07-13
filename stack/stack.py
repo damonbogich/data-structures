51a02bfc83cd49e190a2cf11bc932c2834fe5c97
damonbogich <damonbogich@gmail.com>
2020-07-13 17:55:15 -0400

    list tack implementation working

--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,37 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+#Python list implementation:
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return len(self.storage)
 
     def push(self, value):
-        pass
+        #Add to the top of the stack
+        self.storage.insert(0, value)
 
     def pop(self):
-        pass
+        #removes from the top of the stack
+        if len(self.storage) == 0:
+            return None
+        else:
+            return self.storage.pop(0)
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         # self.storage = ?
+
+#     def __len__(self):
+#         pass
+
+#     def push(self, value):
+#         pass
+
+#     def pop(self):
+#         pass
